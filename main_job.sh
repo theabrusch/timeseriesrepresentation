13b78c1bfde23b90a5fdaf9070d4fade696aabbe
Thea Brusch <s164238@student.dtu.dk>
2023-01-02 10:23:36 +0100

    local changes

--- main_job.sh
+++ main_job.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 #BSUB -q gpuv100
 #BSUB -gpu "num=1"
-#BSUB -J train_selfsupervised
+#BSUB -J train_selfsupervised_no_classifier
 ### number of core
 #BSUB -n 1 
 ### specify that all cores should be on the same host
 #BSUB -R "span[hosts=1]"
 ### specify the memory needed
-#BSUB -R "rusage[mem=200GB]"
+#BSUB -R "rusage[mem=10GB]"
 ### Number of hours needed
 #BSUB -N 
-#BSUB -W 01:00
+#BSUB -W 02:00
 ### added outputs and errors to files
-#BSUB -o logs/Output_selfsuper_%J.out
-#BSUB -e logs/Error_selfsuper%J.err
+#BSUB -o logs/Output_selfsuper_no_class_%J.out
+#BSUB -e logs/Error_selfsuper_no_class_%J.err
 
 module load python3/3.9.11
 
-python3 main.py --data_path /work3/theb/timeseries/ECG/ --epochs 50
+python3 main.py --data_path /work3/theb/timeseries/ECG/ --epochs 50 --train_classifier False
