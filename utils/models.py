254ec40e337e8b0ca2405faa205b410520535d98
theabrusch <62242837+theabrusch@users.noreply.github.com>
2023-01-05 22:02:35 +0100

    fix new loss

--- utils/models.py
+++ utils/models.py
@@ -99,9 +99,8 @@ class ClassifierModule(nn.Module):
 
 class ContrastiveLoss(nn.Module):
 
-    def __init__(self, device, batch_size, temperature, use_cosine_similarity):
+    def __init__(self, device, temperature, use_cosine_similarity = True):
         super(ContrastiveLoss, self).__init__()
-        self.batch_size = batch_size
         self.temperature = temperature
         self.device = device
         self.softmax = torch.nn.Softmax(dim=-1)
@@ -140,31 +139,32 @@ class ContrastiveLoss(nn.Module):
         return v
 
     def forward(self, zis, zjs):
+        batch_size = zis.shape[0]
         representations = torch.cat([zjs, zis], dim=0)
 
         similarity_matrix = self.similarity_function(representations, representations)
 
         # filter out the scores from the positive samples
-        l_pos = torch.diag(similarity_matrix, self.batch_size)
-        r_pos = torch.diag(similarity_matrix, -self.batch_size)
-        positives = torch.cat([l_pos, r_pos]).view(2 * self.batch_size, 1)
+        l_pos = torch.diag(similarity_matrix, batch_size)
+        r_pos = torch.diag(similarity_matrix, -batch_size)
+        positives = torch.cat([l_pos, r_pos]).view(2 * batch_size, 1)
 
-        negatives = similarity_matrix[self.mask_samples_from_same_repr].view(2 * self.batch_size, -1)
+        negatives = similarity_matrix[self.mask_samples_from_same_repr].view(2 * batch_size, -1)
 
         logits = torch.cat((positives, negatives), dim=1)
         logits /= self.temperature
 
         """Criterion has an internal one-hot function. Here, make all positives as 1 while all negatives as 0. """
-        labels = torch.zeros(2 * self.batch_size).to(self.device).long()
+        labels = torch.zeros(2 * batch_size).to(self.device).long()
         CE = self.criterion(logits, labels)
 
-        onehot_label = torch.cat((torch.ones(2 * self.batch_size, 1),torch.zeros(2 * self.batch_size, negatives.shape[-1])),dim=-1).to(self.device).long()
+        onehot_label = torch.cat((torch.ones(2 * batch_size, 1),torch.zeros(2 * batch_size, negatives.shape[-1])),dim=-1).to(self.device).long()
         # Add poly loss
         pt = torch.mean(onehot_label* torch.nn.functional.softmax(logits,dim=-1))
 
-        epsilon = self.batch_size
+        epsilon = batch_size
         # loss = CE/ (2 * self.batch_size) + epsilon*(1-pt) # replace 1 by 1/self.batch_size
-        loss = CE / (2 * self.batch_size) + epsilon * (1/self.batch_size - pt)
+        loss = CE / (2 * batch_size) + epsilon * (1/batch_size - pt)
         # loss = CE / (2 * self.batch_size)
 
         return loss
