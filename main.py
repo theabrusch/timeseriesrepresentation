2bcd50bfa85ae4494193755606f0bbb7bf8378e8
theabrusch <62242837+theabrusch@users.noreply.github.com>
2023-01-09 09:27:35 +0100

    overwrite

--- main.py
+++ main.py
@@ -17,16 +17,17 @@ def main(args):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     output_path = f'{args.output_path}/classifier_{args.train_classifier}_TFC_{args.train_TFC}_abs_budget_{args.abs_budget}_stride_{args.stride}_loss_{args.loss}'
 
-    if not os.path.exists(output_path):
-            os.makedirs(output_path)
-    elif len(os.listdir(output_path)) == 0:
-        output_path = output_path
-    else:
-        i = 1
-        while os.path.exists(output_path + f'_v_{i}') and not len(os.listdir(output_path + f'_v_{i}')) == 0:
-            i+=1
-        output_path = output_path + f'_v_{i}'
-        os.makedirs(output_path, exist_ok=True)
+    if not args.overwrite:
+        if not os.path.exists(output_path):
+                os.makedirs(output_path)
+        elif len(os.listdir(output_path)) == 0:
+            output_path = output_path
+        else:
+            i = 1
+            while os.path.exists(output_path + f'_v_{i}') and not len(os.listdir(output_path + f'_v_{i}')) == 0:
+                i+=1
+            output_path = output_path + f'_v_{i}'
+            os.makedirs(output_path, exist_ok=True)
     print('Saving outputs in', output_path)
     
     if args.pretrain:
@@ -183,6 +184,7 @@ if __name__ == '__main__':
     parser.add_argument('--finetune_path', type = str, default = 'datasets/EMG/')
     parser.add_argument('--batch_size', type = int, default = 128)
     parser.add_argument('--output_path', type = str, default = 'outputs')
+    parser.add_argument('--overwrite', type = eval, default = False)
 
     # augmentation arguments
     parser.add_argument('--abs_budget', type = eval, default = False)
