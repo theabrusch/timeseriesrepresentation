139f7fb6d19457c2633e8d33cca53fc64b9a83b0
theabrusch <62242837+theabrusch@users.noreply.github.com>
2023-02-09 09:00:56 +0100

    fix augmentations

--- utils/dataset.py
+++ utils/dataset.py
@@ -45,7 +45,7 @@ class TFC_Dataset(Dataset):
         channels, time_length = get_dset_info(X = X, dset = dset, sample_channel = sample_channel)
 
         if time_length > X.shape[2]:
-             X = F.pad(X[:,:channels,:], (0,int(206-X.shape[2])))
+             X = F.pad(X[:,:channels,:], (0,int(time_length-X.shape[2])))
         else:
             X = X[:,:channels,:time_length]
 
@@ -68,14 +68,17 @@ class TFC_Dataset(Dataset):
 
         self.X_f = torch.fft.fft(self.X_t, axis = -1).abs()
         if not test_mode and not fine_tune_mode:
-            self.X_f_aug = frequency_augmentation(self.X_f, keep_all = False, abs_budget=abs_budget, return_ifft=False)
-            self.X_t_aug = time_augmentation(self.X_t, keep_all = False, return_fft=False)
+            config = {
+                'jitter_scale_ratio': 1.1,
+                'jitter_ratio': 0.8,
+                'max_seg': 8
+            }
+            self.X_f_aug = frequency_augmentation(self.X_f, config)
+            self.X_t_aug = time_augmentation(self.X_t, config)
     
     def __getitem__(self, idx):
-        freq_aug = torch.randint(high = 2, size = [1])
-        time_aug = torch.randint(high = 3, size = [1])
         if not self.test_mode and not self.fine_tune_mode:
-            return self.X_t[idx], self.X_f[idx], self.X_t_aug[idx][time_aug].squeeze(0), self.X_f_aug[idx][freq_aug].squeeze(0), self.Y[idx]
+            return self.X_t[idx], self.X_f[idx], self.X_t_aug[idx], self.X_f_aug[idx], self.Y[idx]
         elif self.fine_tune_mode:
             return self.X_t[idx], self.X_f[idx], self.X_t[idx], self.X_f[idx], self.Y[idx]
         else:
