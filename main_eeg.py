fd94cd9a82af313b8fe118fbf1eb2b3ec60a0b51
theabrusch <62242837+theabrusch@users.noreply.github.com>
2023-02-13 14:16:19 +0100

    don't overwrite

--- main_eeg.py
+++ main_eeg.py
@@ -23,12 +23,10 @@ def params_to_tb(writer, args):
 
 def check_output_path(output_path):
     if not os.path.exists(output_path):
-                os.makedirs(output_path)
-    elif len(os.listdir(output_path)) == 0:
-        output_path = output_path
+        os.makedirs(output_path)
     else:
         i = 1
-        while os.path.exists(output_path + f'_v_{i}') and not len(os.listdir(output_path + f'_v_{i}')) == 0:
+        while os.path.exists(output_path + f'_v_{i}'):
             i+=1
         output_path = output_path + f'_v_{i}'
         os.makedirs(output_path, exist_ok=True)
