1bc5e358d01d330236f1d911ec0a6181ab47c895
Thea Brusch <s164238@student.dtu.dk>
2023-04-17 21:38:05 +0200

    job scripts

--- main_job_multiview.sh
+++ main_job_multiview.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #BSUB -q gpuv100
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -J multiview_pretrain_flatten_false
+#BSUB -J multiview_pretrain_CMC
 ### number of core
 #BSUB -n 1 
 ### specify that all cores should be on the same host
@@ -12,9 +12,10 @@
 #BSUB -N 
 #BSUB -W 24:00
 ### added outputs and errors to files
-#BSUB -o logs/Output_multiview_pretrain_flatten_false_%J.out
-#BSUB -e logs/Error_multiview_pretrain_flatten_false_%J.err
+#BSUB -o logs/Output_multiview_pretrain_CMC_%J.out
+#BSUB -e logs/Error_multiview_pretrain_CMC_%J.err
 
 module load python3/3.9.11
+source timeseries-env/bin/activate
 
-python3 main_multiview.py --data_path sleepeeg.yml --finetune_path 'same' --pretrain_epochs 20 --finetune_epochs 40 --batchsize 32 --load_model False --save_model True --pretrain True --finetune False --optimize_encoder True --learning_rate 1e-3 --pool 'max' --choose_best True --evaluate_latent_space False --multi_channel_setup 'sample_channel' --sample_pretrain_subjects False --sample_finetune_train_subjects 16 --sample_finetune_val_subjects 4 --target_batchsize 32 --encoder 'wave2vec' --ft_learning_rate 1e-4 --sample_test_subjects False --flatten False --weight_decay 1e-2 --balanced_sampling 'finetune' --feat_do 0.4 --hidden_channels 256 --output_path 'pretrained_outputs'
+python3 main_multiview.py --data_path sleepeeg.yml --finetune_path 'sleepedf.yml' --pretrain_epochs 20 --finetune_epochs 40 --batchsize 32 --load_model False --save_model True --pretrain True --finetune False --optimize_encoder True --learning_rate 1e-3 --pool 'max' --choose_best True --evaluate_latent_space False --multi_channel_setup 'sample_channel' --sample_pretrain_subjects False --sample_finetune_train_subjects 16 --sample_finetune_val_subjects 4 --target_batchsize 32 --encoder 'wave2vec' --ft_learning_rate 1e-4 --sample_test_subjects False --weight_decay 1e-2 --balanced_sampling 'finetune' --feat_do 0.4 --hidden_channels 256 --output_path 'pretrained_outputs' --loss 'timeloss' --pretraining_setup 'GNN' --num_message_passing_rounds 1
--- train_sleepedf.sh
+++ train_sleepedf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #BSUB -q gpuv100
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -J multiview_scratch_sleepedf_lowreg
+#BSUB -J ft_sleepedf_100_gnn_highlr
 ### number of core
 #BSUB -n 1 
 ### specify that all cores should be on the same host
@@ -10,11 +10,12 @@
 #BSUB -R "rusage[mem=256GB]"
 ### Number of hours needed
 #BSUB -N 
-#BSUB -W 05:00
+#BSUB -W 01:00
 ### added outputs and errors to files
-#BSUB -o logs/Output_multiview_scratch_sleepedf_%J.out
-#BSUB -e logs/Error_multiview_scratch_sleepedf_%J.err
+#BSUB -o logs/Output_multiview_ft_sleepedf_100_cocoa_%J.out
+#BSUB -e logs/Error_multiview_ft_sleepedf_100_cocoa_%J.err
 
 module load python3/3.9.11
+source timeseries-env/bin/activate
 
-python3 main_multiview.py --data_path sleepeeg.yml --finetune_path sleepedf.yml --pretrain_epochs 20 --finetune_epochs 40 --batchsize 32 --load_model False --save_model False --pretrain False --finetune True --optimize_encoder False --learning_rate 1e-3 --pool 'max' --choose_best True --evaluate_latent_space False --multi_channel_setup 'sample_channel' --sample_pretrain_subjects False --sample_finetune_train_subjects 16 --sample_finetune_val_subjects 4 --target_batchsize 32 --encoder 'wave2vec' --ft_learning_rate 1e-3 --sample_test_subjects False --flatten False --weight_decay 1e-4 --balanced_sampling 'finetune' --feat_do 0.1 --hidden_channels 256 --layers 2 --num_message_passing_rounds 2 --output_path 'pretrained_outputs' --pretrained_model_path 'pretrained_outputs/MultiView_sleepeeg_pretrain_True_pretrain_subjs_False_multi_channel_setup_sample_channel_v_5/pretrained_model.pt' --seed_generator True
+python3 main_multiview.py --data_path sleepeeg.yml --finetune_path sleepedf.yml --pretrain_epochs 20 --finetune_epochs 40 --batchsize 32 --load_model True --save_model False --pretrain False --finetune True --optimize_encoder True --learning_rate 1e-3 --pool 'max' --choose_best True --evaluate_latent_space False --multi_channel_setup 'sample_channel' --sample_pretrain_subjects False --sample_finetune_train_subjects 16 --sample_finetune_val_subjects 4 --target_batchsize 32 --encoder 'wave2vec' --ft_learning_rate 3e-3 --sample_test_subjects False --weight_decay 1e-2 --balanced_sampling 'finetune' --feat_do 0.4 --hidden_channels 256 --layers 6 --num_message_passing_rounds 3 --output_path 'pretrained_outputs' --pretrained_model_path 'pretrained_models/MultiView_sleepeeg_big_COCOA/pretrained_model.pt' --seed_generator 100 --conv_do 0.1 --pretraining_setup 'COCOA'
