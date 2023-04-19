28929162fd420d81dbc03235f86c6882cddf1826
theabrusch <62242837+theabrusch@users.noreply.github.com>
2023-04-19 09:39:08 +0200

    load loss correctly

--- utils/multiview.py
+++ utils/multiview.py
@@ -368,10 +368,10 @@ def load_model(pretraining_setup, device, channels, time_length, num_classes, mo
     if model_args.loss == 'time_loss':
         loss_fn = TS2VecLoss(alpha = 0.5, temporal_unit = 0).to(device)
     elif model_args.loss == 'contrastive':
-        loss_fn = ContrastiveLoss(device, tau = 0.5).to(device)
+        loss_fn = ContrastiveLoss(temperature = 0.5).to(device)
     elif model_args.loss == 'COCOA':
         loss_fn = COCOAloss(temperature = 0.5).to(device)
     elif model_args.loss == 'CMC':
-        loss_fn = CMCloss(device, temperature = 0.5).to(device)
+        loss_fn = CMCloss(temperature = 0.5).to(device)
 
     return model, loss_fn
