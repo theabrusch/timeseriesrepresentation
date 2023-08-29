ca721c916317ab1729fd39ef2db08682204cfd3e
theabrusch <62242837+theabrusch@users.noreply.github.com>
2023-08-29 20:43:36 +0200

    update configs

--- sh/pull_runs.sh
+++ sh/pull_runs.sh
@@ -1,1 +1,1 @@
-rsync -Pav -e ssh theb@login.gbar.dtu.dk:/zhome/89/a/117273/Desktop/alignement_of_representations/outputs/no_bckg/ ~/Documents/PhD/code/alignement_of_representations/outputs/no_bckg/
+rsync -Pav -e ssh theb@login.gbar.dtu.dk:/zhome/89/a/117273/Desktop/alignement_of_representations/outputs/ ~/Documents/PhD/code/alignement_of_representations/outputs/
